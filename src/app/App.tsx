684ee554ec0bc209d68614ed264d5cb09a4908e7
최정혁 <choejeonghyeog@choejeonghyeogs-MacBook-Air.local>
2026-03-31 12:24:11 +0900

    chore(#1): 디렉토리 및 파일 구조 설정 완료

---
+++ src/app/App.tsx
@@ -0,0 +1,17 @@
+import Banner from "../widgets/banner";
+import Header from "../widgets/header";
+import Layout from "../widgets/layout";
+
+function App() {
+
+  return (
+    <>
+   <Header/>
+    <Banner/>
+    <Layout>
+    </Layout>
+    </>
+  );
+}
+
+export default App
---
+++ src/widgets/layout.tsx
@@ -0,0 +1,9 @@
+const Layout = () => {
+  return(
+		<section>
+      adad
+    </section>
+  );
+}
+
+export default Layout;
